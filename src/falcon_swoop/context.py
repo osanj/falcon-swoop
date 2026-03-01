64a6931fd782d2fde01814146f88689ba13c4f84
Jonas Schuepfer <jonasschuepfer@gmail.com>
2026-03-01 18:19:34 +0100

    Adding documentation to smaller classes

--- src/falcon_swoop/context.py
+++ src/falcon_swoop/context.py
@@ -5,11 +5,23 @@ import falcon.asgi
 
 @dataclass
 class OpContext:
+    """Utility class for direct access to falcon data structures if needed.
+
+    This context will be injected to the function decorated with ``@operation`` as argument that hints this type.
+    Note that it can only be used in synchronous operations.
+    """
+
     req: falcon.Request
     resp: falcon.Response
 
 
 @dataclass
 class OpAsgiContext:
+    """Utility class for direct access to falcon async data structures if needed.
+
+    This context will be injected to the function decorated with ``@operation`` as argument that hints this type.
+    Note that it can only be used in asynchronous operations.
+    """
+
     req: falcon.asgi.Request
     resp: falcon.asgi.Response
--- src/falcon_swoop/output.py
+++ src/falcon_swoop/output.py
@@ -11,6 +11,12 @@ T = TypeVar("T", bound=BaseModel | OpBinary | OpAsgiBinary | None)
 
 
 class OpOutput(Generic[T]):
+    """Operation output wrapper for more control.
+
+    Generally falcon-swoop operations return pydantic models or binary data directly. However, one may want to control
+    other parts of the response, such as status code, content type and headers. For this OpOutput[<...>] can be used.
+    """
+
     def __init__(
         self,
         payload: T,
@@ -21,6 +27,18 @@ class OpOutput(Generic[T]):
         expires: str | datetime | None = None,
         headers: dict[str, str] | None = None,
     ):
+        """Create falcon-swoop operation output.
+
+        :param payload: data that will go in the http body, can be pydantic model, binary or None
+        :param status_code: optional status code, if ``None`` falcon-swoop will use the default status code
+            configured in ``@operation(default_status=...)``
+        :param content_type: optional content type, if ``None`` falcon-swoop will determine the content type
+            based on the payload type or based on configuration if ``@operation(response_content_type=...)`` is used
+        :param cache_control: optional cache-control header to control caching
+        :param etag: optional etag header to control caching
+        :param expires: optional expires header to control caching
+        :param headers: any additional headers that shall be set on the response
+        """
         self.payload = payload
         self.status_code = status_code
         self.content_type = content_type
@@ -31,6 +49,7 @@ class OpOutput(Generic[T]):
 
     @property
     def payload(self) -> T:
+        """Data that will go in the HTTP body."""
         return self.__payload
 
     @payload.setter
