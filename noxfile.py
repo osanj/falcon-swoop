2f7eed8267f9be146cf4e1d8c6500f9533e9ee40
Jonas Schuepfer <jonasschuepfer@gmail.com>
2026-03-02 23:55:10 +0100

    Simple parametrized setup with pydantic versions

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,27 @@
 import nox
 
-PY_VERSIONS = ["3.10", "3.12"]
-# PY_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
-
-
-# @nox.session(python=PY_VERSIONS)
-# def install(session: nox.Session):
-#     session.install("-e", ".[dev]")
+PY_VERSIONS = ["3.10", "3.12"]  # ["3.10", "3.11", "3.12", "3.13"]
+PYDANTIC_NEWEST = "2"
+PYDANTIC_VERSIONS = [PYDANTIC_NEWEST, "2.12", "2.11", "2.10", "2.9", "2.6", "2.4", "2.0"]
 
 
 @nox.session(python=PY_VERSIONS)
-def matrix(session: nox.Session):
+@nox.parametrize("pydantic", PYDANTIC_VERSIONS)
+def matrix(session: nox.Session, pydantic: str):
     session.install("-e", ".[dev]")
 
-    session.run("ruff", "check")
-    session.run("ruff", "format", "--check")
-    session.run("mypy")
+    verbose = "verbose" in session.posargs
 
-    session.run("pytest", "-v")
+    # run lint only on main version
+    if pydantic == PYDANTIC_NEWEST:
+        session.run("ruff", "check")
+        session.run("ruff", "format", "--check")
+        session.run("mypy")
+
+    else:
+        session.install(f"pydantic=={pydantic}")
+
+    pytest_args = ["pytest"]
+    if verbose:
+        pytest_args.append("v")
+    session.run(*pytest_args)
